716f8e147251a1ebbc265a895276ef4cbd619e53
Andrew Ting <andrewt.7077@gmail.com>
2021-12-12 21:09:31 +1100

    Add functions to complete part 2

--- 2021/day1_sonar_sweep/sonar_sweep.py
+++ 2021/day1_sonar_sweep/sonar_sweep.py
@@ -34,16 +34,46 @@ def sonar_sweep(measurements:list) -> int:
     """
     n_increases = 0
     
-    tmp = int(measurements[0])
+    tmp = measurements[0]
     # Iterate through every measurement and compare to previous
     for m in measurements:
-        m = int(m)
         if m > tmp:
             n_increases += 1
         tmp = m
     
     return n_increases
+
+
+def rolling_sum(measurements:list,
+                rolling_window:int = 3,
+                ) -> list:
+    """Convert measurements into a rolling sum list.
     
+    Parameters
+    ----------
+    measurements : list
+        DESCRIPTION.
+
+    Returns
+    -------
+    list
+        DESCRIPTION.
+
+    """
+    measurements_rs = []
+    
+    #
+    for i in range(len(measurements) - rolling_window + 1):
+        window = measurements[i:i+rolling_window]
+        measurements_rs.append(sum(window))
+        
+    return measurements_rs
+
+
+def str_to_int(lst:list) -> list:
+    """Convert string items in list to int."""
+    return [int(item) for item in lst]
+
 
 # %%
 
@@ -52,5 +82,7 @@ if __name__ == "__main__":
         depth_measurements = f.read()
         # Slice out final entry as it is an empty line
         list_dm = depth_measurements.split("\n")[:-1]
+        list_dm = str_to_int(list_dm)
         print(sonar_sweep(list_dm))
+        print(sonar_sweep(rolling_sum(list_dm)))
         
