38978357b542911085f6444e81ae39b0e71ff974
Andrew Ting <andrewt.7077@gmail.com>
2021-12-13 22:08:17 +1100

    Initialise day 2 puzzle files

---
+++ 2021/day2_dive/dive.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2021 - Day 2 Dive!
+
+@author: Andrew Ting
+@last-modified: 13/12/21
+"""
+
+
+# %%
+
+def dive():
+    """
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+# %%
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        pass
+        
