eb4e324c3c629a5251f93e3878440e2649b8b057
Andrew Ting <andrewt.7077@gmail.com>
2022-07-10 11:04:58 +1000

    Initialise day 3 files

---
+++ 2021/day3_binary_diagnostic/binary_diagnostic.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2021 - Day 3: Binary Diagnostic
+
+@author: Andrew Ting
+@last-modified: 2022/07/10
+"""
+
+
+# %%
+
+
+# %%
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        pass
