2309470dee554caaacfbb02b7c6652b42e90cbc0
Andrew Ting <andrewt.7077@gmail.com>
2022-07-10 11:02:36 +1000

    Complete part 2

--- 2021/day2_dive/dive.py
+++ 2021/day2_dive/dive.py
@@ -10,7 +10,12 @@ Advent of Code 2021 - Day 2: Dive!
 # %%
 
 def dive(instructions: list) -> list:
-    """Calculate the horizontal position and depth after planned course.
+    """Calculate the horizontal position and depth using instructions.
+
+    Possible course instructions:
+    'forward X': increases the horizontal position by X units.
+    'down X'   : increases the depth by X units.
+    'up X'     : decreases the depth by X units.
 
     Parameters
     ----------
@@ -40,6 +45,45 @@ def dive(instructions: list) -> list:
     return (horizontal_position, depth)
 
 
+def updated_dive(instructions: list) -> list:
+    """Calculate the horizontal position and depth using updated instructions.
+
+    Possible course instructions:
+    'forward X': increases the horizontal position by X units and increases the
+                 depth by aim multiplied by X units.
+    'down X'   : increases the aim by X units.
+    'up X'     : decreases the aim by X units.
+
+    Parameters
+    ----------
+    instructions : list
+        List of instructions for the submarine to follow.
+
+    Returns
+    -------
+    horizontal_position, depth : tuple
+        The horizontal position and depth of the submarine after following the
+        updated planned course.
+
+    """
+    horizontal_position = 0
+    depth = 0
+    aim = 0
+
+    for ins in instructions:
+        direction, units = ins.split(" ")
+        move = int(units)
+        if direction == "forward":
+            horizontal_position += move
+            depth += aim * move
+        elif direction == "down":
+            aim += move
+        elif direction == "up":
+            aim -= move
+
+    return (horizontal_position, depth)
+
+
 # %%
 
 if __name__ == "__main__":
@@ -54,5 +98,5 @@ if __name__ == "__main__":
         print(f"Part 1 solution: {part1_output[0] * part1_output[1]}")
 
         # Part 2
-        # part2_output = dive(list_ci)
-        # print(f"Part 2 solution: {part2_output}")
+        part2_output = updated_dive(list_ci)
+        print(f"Part 2 solution: {part2_output[0] * part2_output[1]}")
