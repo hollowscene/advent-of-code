0955682272a31216f2485b800c72c7b57d65e2de
Andrew Ting <andrewt.7077@gmail.com>
2022-07-10 10:56:02 +1000

    Complete part 1

--- 2021/day2_dive/dive.py
+++ 2021/day2_dive/dive.py
@@ -1,30 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-Advent of Code 2021 - Day 2 Dive!
+Advent of Code 2021 - Day 2: Dive!
 
 @author: Andrew Ting
-@last-modified: 13/12/21
+@last-modified: 2022/07/10
 """
 
 
 # %%
 
-def dive():
-    """
+def dive(instructions: list) -> list:
+    """Calculate the horizontal position and depth after planned course.
 
     Parameters
     ----------
+    instructions : list
+        List of instructions for the submarine to follow.
 
     Returns
     -------
+    horizontal_position, depth : tuple
+        The horizontal position and depth of the submarine after following the
+        planned course.
 
     """
-    pass
+    horizontal_position = 0
+    depth = 0
+
+    for ins in instructions:
+        direction, units = ins.split(" ")
+        move = int(units)
+        if direction == "forward":
+            horizontal_position += move
+        elif direction == "down":
+            depth += move
+        elif direction == "up":
+            depth -= move
+
+    return (horizontal_position, depth)
 
 
 # %%
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
-        pass
-        
+        course_instructions = f.read()
+
+        # Slice out final entry as it is an empty line
+        list_ci = course_instructions.split("\n")[:-1]
+
+        # Part 1
+        part1_output = dive(list_ci)
+        print(f"Part 1 solution: {part1_output[0] * part1_output[1]}")
+
+        # Part 2
+        # part2_output = dive(list_ci)
+        # print(f"Part 2 solution: {part2_output}")
