6d8dd377b3ccf7a9a6999bae1d8320640d200dc9
Andrew Ting <andrewt.7077@gmail.com>
2022-12-05 22:54:37 +1100

    Simplify logic

--- advent_of_code/2022/day5_supply_stacks/supply_stacks.py
+++ advent_of_code/2022/day5_supply_stacks/supply_stacks.py
@@ -30,49 +30,51 @@ def _parse_crates_at_height(crates: str, n_stacks: int) -> list:
 
 def main(input_path: str, part: int) -> int:
     """Solve today's Advent of Code problems."""
+    assert part in [1, 2], "Part must be 1 or 2"
+
     input_list = load.read_input_to_list(input_path)
     solution = None
 
+    # Seperate input
+    crate_input = []
+    procedure_input = []
+
+    instructions_switch = 0
+    for line in input_list:
+        if line == "":
+            instructions_switch = 1
+            continue
+
+        if instructions_switch:
+            procedure_input.append(line)
+        else:
+            crate_input.append(line)
+
+    # Parse crate stack
+    crate_indices = crate_input[-1][1:-1].split("   ")
+    n_stacks = int(crate_indices[-1])
+
+    crate_stack = []
+    for _ in range(n_stacks):
+        crate_stack.append([])
+    
+    # Consider the bottom-most crate as height of 0
+    # Each list corresponds to a stack from 1 to 9
+    # Each item in the list is a crate from bottom to top
+    for height in range(len(crate_input) - 1):
+        crates_at_height = _parse_crates_at_height(crate_input[-height-2], n_stacks)
+        for i in range(len(crates_at_height)):
+            crate_stack[i].append(crates_at_height[i])
+
+    # Remove empty crates from top of stacks
+    for stack in crate_stack:
+        while stack[-1] == " ":
+            stack.pop()
+
+    # print(crate_stack)
+
+    # Execute reassignment procedure
     if part == 1:
-        # Seperate input
-        crate_input = []
-        procedure_input = []
-
-        instructions_switch = 0
-        for line in input_list:
-            if line == "":
-                instructions_switch = 1
-                continue
-
-            if instructions_switch:
-                procedure_input.append(line)
-            else:
-                crate_input.append(line)
-
-        # Parse crate stack
-        crate_indices = crate_input[-1][1:-1].split("   ")
-        n_stacks = int(crate_indices[-1])
-
-        crate_stack = []
-        for _ in range(n_stacks):
-            crate_stack.append([])
-        
-        # Consider the bottommost crate as height of 0
-        # Each list corresponds to a stack from 1 to 9
-        # Each item in the list is a crate from bottom to top
-        for height in range(len(crate_input) - 1):
-            crates_at_height = _parse_crates_at_height(crate_input[-height-2], n_stacks)
-            for i in range(len(crates_at_height)):
-                crate_stack[i].append(crates_at_height[i])
-
-        # Remove empty crates from top of stacks
-        for stack in crate_stack:
-            while stack[-1] == " ":
-                stack.pop()
-
-        # print(crate_stack)
-
-        # Execute reassignment procedure
         for move in procedure_input:
             number_of_moves, from_stack, to_stack = re.findall(r"\d+", move)
 
@@ -84,53 +86,8 @@ def main(input_path: str, part: int) -> int:
                 moved_crate = crate_stack[from_stack].pop()
                 crate_stack[to_stack].append(moved_crate)
 
-        # print(crate_stack)
-
-        solution = ""
-        for stack in crate_stack:
-            solution += stack[-1]
-
-    elif part == 2:
-        # Seperate input
-        crate_input = []
-        procedure_input = []
-
-        instructions_switch = 0
-        for line in input_list:
-            if line == "":
-                instructions_switch = 1
-                continue
-
-            if instructions_switch:
-                procedure_input.append(line)
-            else:
-                crate_input.append(line)
-
-        # Parse crate stack
-        crate_indices = crate_input[-1][1:-1].split("   ")
-        n_stacks = int(crate_indices[-1])
-
-        crate_stack = []
-        for _ in range(n_stacks):
-            crate_stack.append([])
-        
-        # Consider the bottommost crate as height of 0
-        # Each list corresponds to a stack from 1 to 9
-        # Each item in the list is a crate from bottom to top
-        for height in range(len(crate_input) - 1):
-            crates_at_height = _parse_crates_at_height(crate_input[-height-2], n_stacks)
-            for i in range(len(crates_at_height)):
-                crate_stack[i].append(crates_at_height[i])
-
-        # Remove empty crates from top of stacks
-        for stack in crate_stack:
-            while stack[-1] == " ":
-                stack.pop()
-
-        # print(crate_stack)
-
-        # Execute new reassignment procedure
-        run_count = 0
+    # Execute new reassignment procedure
+    else:
         for move in procedure_input:
             number_of_moves, from_stack, to_stack = re.findall(r"\d+", move)
 
@@ -141,14 +98,11 @@ def main(input_path: str, part: int) -> int:
             crate_stack[to_stack] = crate_stack[to_stack] + crate_stack[from_stack][-number_of_moves:]
             crate_stack[from_stack] = crate_stack[from_stack][:-number_of_moves]
 
-        # print(crate_stack)
+    # print(crate_stack)
 
-        solution = ""
-        for stack in crate_stack:
-            solution += stack[-1]
-
-    else:
-        raise ValueError
+    solution = ""
+    for stack in crate_stack:
+        solution += stack[-1]
 
     return solution
 
@@ -156,10 +110,10 @@ def main(input_path: str, part: int) -> int:
 # %%
 
 if __name__ == "__main__":
-    input_path = "advent_of_code\\2022\\day5_supply_stacks\\input.txt"
+    full_input_path = load.get_full_file_path("input.txt")
 
-    part1_solution = main(input_path, 1)
+    part1_solution = main(full_input_path, 1)
     print(f"Part 1 solution: {part1_solution}")
 
-    part2_solution = main(input_path, 2)
+    part2_solution = main(full_input_path, 2)
     print(f"Part 2 solution: {part2_solution}")
