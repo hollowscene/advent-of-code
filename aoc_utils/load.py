68851086e91b97cc99dd94dbb578bc09d3409e77
Andrew Ting <andrewt.7077@gmail.com>
2022-12-05 22:54:07 +1100

    Add function to derive file path

--- aoc_utils/load.py
+++ aoc_utils/load.py
@@ -3,22 +3,37 @@
 Common functions to load Advent Of Code inputs.
 
 @author: Andrew Ting
-@last-modified: 2022/12/01
+@last-modified: 2022/12/05
 """
 
+import inspect
 import os
+from pathlib import Path
 
 
 # %%
 
-def read_input_to_list(relative_path: str) -> list:
-    """Load Advent of Code problem input and return contents as a list."""
-    cwd = os.getcwd()
-    if relative_path[:2] != "\\":
-        relative_path = "\\" + relative_path
-    full_path_to_input = cwd + relative_path
+def get_full_file_path(relative_path: str = None) -> os.path:
+    """Convert a relative file path to an absolute file path."""
+    # Use stack to find path of caller
+    # https://stackoverflow.com/questions/55469447/get-caller-functions-absolute-path-to-its-file
+    # Apparently this isn't best practice but I haven't been able to get any 
+    # other solutions to work
+    # See https://stackoverflow.com/questions/2654113/how-to-get-the-callers-method-name-in-the-called-method
+    frame = inspect.stack()[1]
+    p = frame[0].f_code.co_filename
 
-    with open(full_path_to_input, "r") as f:
+    if relative_path is None:
+        full_path_to_input = Path(p)
+    else:
+        full_path_to_input = os.path.join(Path(p).parent, relative_path)
+
+    return full_path_to_input
+
+
+def read_input_to_list(file_path: str) -> list:
+    """Load Advent of Code problem input and return contents as a list."""
+    with open(file_path, "r") as f:
         stream = f.read()
 
         # Convert to list split by new lines and slice out final item as it 
